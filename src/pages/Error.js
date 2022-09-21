5ffe2ec686e024975ba14d1fa3717773e04f9589
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-21 05:13:56 +0300

    yarn.build

---
+++ src/pages/Error.js
@@ -0,0 +1,9 @@
+import Styles from './Error.module.scss';
+
+export const Error = () => {
+  return (
+    <div className={Styles.main}>
+      <h1>Check where you are going MATE</h1>
+    </div>
+  );
+};
