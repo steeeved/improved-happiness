ee70c3c60b1ed279ca6f5ce61758f6605625acb8
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-21 05:13:33 +0300

    yarn.build

---
+++ src/config/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDzEG6w8FLB3TuorYHWZTsygCP0ZYZtfaM',
+  authDomain: 'meme-1a200.firebaseapp.com',
+  projectId: 'meme-1a200',
+  storageBucket: 'meme-1a200.appspot.com',
+  messagingSenderId: '970207178500',
+  appId: '1:970207178500:web:cf20fb0412d6d65fcf6a51'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
