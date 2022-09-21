36f6499063d9fde815e03b1e57fb788eb69391c3
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-21 06:24:35 +0300

    Hope this works

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -1,17 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDzEG6w8FLB3TuorYHWZTsygCP0ZYZtfaM',
-  authDomain: 'meme-1a200.firebaseapp.com',
-  projectId: 'meme-1a200',
-  storageBucket: 'meme-1a200.appspot.com',
-  messagingSenderId: '970207178500',
-  appId: '1:970207178500:web:cf20fb0412d6d65fcf6a51'
+  apiKey: 'AIzaSyCuf5aaFPxxKjMXD8AaDv0dqs8bgppc0qM',
+  authDomain: 'meme-d50a0.firebaseapp.com',
+  projectId: 'meme-d50a0',
+  storageBucket: 'meme-d50a0.appspot.com',
+  messagingSenderId: '45155818900',
+  appId: '1:45155818900:web:f80f1f1bcea16a01870381'
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
