5488403fc4a85f8f2d62d6aa2d5aa3bb312d9239
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-21 05:12:00 +0300

    yarn.build

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './context/userContext';
+
+import App from './App';
+import './index.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  //<React.StrictMode>
+  <BrowserRouter>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </BrowserRouter>
+  //</React.StrictMode>
+);
